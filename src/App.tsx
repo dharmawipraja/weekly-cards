c9f9a5f970d7c63963d61bd83793db65b633852a
I Made Budi Dharma Wipraja <v-i.mbwipraja@btpn.com>
2024-12-02 16:22:55 +0700

    Setup radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-function App() {
+import { Theme, Flex, Text, Box, Card } from "@radix-ui/themes";
 
+function App() {
   return (
-    <>
-      <p className="text-blue-600">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Theme>
+      <Flex direction="column" gap="2">
+        <Box maxWidth="350px">
+          <Card asChild>
+            <div>
+              <Text as="div" size="2" weight="bold">
+                Quick start
+              </Text>
+              <Text as="div" color="gray" size="2">
+                Start building your next project in minutes
+              </Text>
+            </div>
+          </Card>
+        </Box>
+      </Flex>
+    </Theme>
   )
 }
 
